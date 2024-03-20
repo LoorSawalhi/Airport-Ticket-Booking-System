e5741ccd54fed8c3b03ace667784304a21131442
LoorSawalhi <loorwael@yahoo.com>
2024-03-20 11:05:19 +0200

    Adding convigurations for dependency injection

--- UserInterface/ServiceConfiguration.cs
+++ UserInterface/ServiceConfiguration.cs
@@ -12,17 +12,22 @@ public static class ServiceConfiguration
     {
         var services = new ServiceCollection();
         var basePath = "/home/loor/Desktop/Foothill Training/C#/AirportTicketBookingSystem/Infrastructure/";
+
         // Register repositories with their file paths
         services.AddSingleton<IPassengerRepository>(new PassengerRepository($"{basePath}passengers.csv"));
-        services.AddSingleton<IAirportRepository>(new AirportRepository("/path/to/airport.csv"));
-        services.AddSingleton<IFlightRepository>(new FlightRepository("/path/to/flights.csv"));
-        services.AddSingleton<IRClassFlightRepository>(new RClassFlightRepository("/path/to/flight_classes.csv"));
+        services.AddSingleton<IAirportRepository>(new AirportRepository($"{basePath}airport.csv"));
+        services.AddSingleton<IFlightRepository>(new FlightRepository($"{basePath}flights.csv"));
+        services.AddSingleton<IRClassFlightRepository>(new RClassFlightRepository($"{basePath}flight_classes.csv"));
+        services.AddSingleton<IBookingRepository>(new BookingRepository($"{basePath}flight_classes.csv"));
+        services.AddSingleton<IClassRepository>(new ClassRepository($"{basePath}flight_classes.csv"));
 
         // Register services
         services.AddSingleton<IPassengerService, PassengerService>();
         services.AddSingleton<IAirportService, AirportService>();
         services.AddSingleton<IFlightService, FlightService>();
         services.AddSingleton<IRClassFlightService, RClassFlightService>();
+        services.AddSingleton<IFlightClassService, FlightClassService>();
+        services.AddSingleton<IBookingService, BookingService>();
 
         return services;
     }
